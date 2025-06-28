03c65b6e06e27d16dc886d1d1f6d853c57036fb5
mikuull <mmikulski643@gmail.com>
2025-06-28 21:09:30 +0200

    refactor(api): add frontend cors to nestjs app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,6 +11,11 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
